32914598e677a8be8571a6e2d56a90e978f75603
tarran <dotfiles@demo.com>
2017-01-19 19:45:29 +0000

    updated tests

--- test.js
+++ test.js
@@ -3,14 +3,17 @@ import m from './';
 
 test(t => {
 	t.deepEqual(m({}), {});
-	t.deepEqual(m({foo: []}), {foo: []});
-	t.deepEqual(m({foo: ['a', 'b']}), {foo: ['a', 'b']});
+	t.deepEqual(m({foo: []}), {});
+	t.deepEqual(m({foo: ['a', 'b']}), {
+		'foo.0': 'a',
+		'foo.1': 'b'
+	});
 	t.deepEqual(m({foo: {bar: ['a', 'b']}}), {
 		'foo.bar.0': 'a',
 		'foo.bar.1': 'b'
 	});
 	t.deepEqual(m({foo: {bar: ['a', {foo: ['b']}]}}), {
 		'foo.bar.0': 'a',
-		'foo.bar.1.foo': 'b'
+		'foo.bar.1.foo.0': 'b'
 	});
 });
